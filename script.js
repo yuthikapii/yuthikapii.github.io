d7cf28337cd36d592bde1aa911f28d0a10e95ad5
yuthikapii <yuthika.gopin@gmail.com>
2026-08-19 17:30:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+window.addEventListener("load", () => {
+    const heroText = document.querySelector(".hero-text");
+    heroText.style.opacity = "0";
+    heroText.style.transition = "opacity 2s ease";
+
+    setTimeout(() => {
+        heroText.style.opacity = "1";
+    }, 300);
+});
 
